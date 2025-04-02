dd605ff0736c431e06ec2868c75264de2d01331d
poojaG16 <poojagaikwad28603@gmail.com>
2025-04-02 22:55:27 +0530

    github link added with UI enhancement

--- my-portfolio/src/app/pages/projects/projects.component.ts
+++ my-portfolio/src/app/pages/projects/projects.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  selectedProject: number | null = null;
 
+  toggleProjectDetails(projectId: number): void {
+    this.selectedProject = this.selectedProject === projectId ? null : projectId;
+  }
 }
